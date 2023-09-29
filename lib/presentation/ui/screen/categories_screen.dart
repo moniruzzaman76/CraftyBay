9294693cb12cb3c15c454cc9cd94b7086ce1565c
moniruzzaman76 <ariyankhanmonir555@gmail.com>
2023-09-29 17:23:35 +0600

    category screen has been done

--- lib/presentation/ui/screen/categories_screen.dart
+++ lib/presentation/ui/screen/categories_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/State_holders/category_controller.dart';
 import 'package:flutter_ecommerce/State_holders/main_botom_nav_controller.dart';
 import 'package:get/get.dart';
+import '../widgets/category_card.dart';
 
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({Key? key}) : super(key: key);
@@ -29,17 +31,24 @@ class CategoryScreen extends StatelessWidget {
         ),
         body: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: GridView.builder(
-            itemCount: 12,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 4,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-              ) ,
-              itemBuilder: (context, index){
-                 // return const CategoryCard();
-                return SizedBox();
-              }
+          child: GetBuilder<CategoryController>(
+            builder: (categoryController) {
+              return Visibility(
+                visible: !categoryController.categoryInProgress,
+                replacement: const Center(child: CircularProgressIndicator(),),
+                child: GridView.builder(
+                  itemCount: categoryController.categoryModel.data?.length ?? 0,
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4,
+                      crossAxisSpacing: 16,
+                      mainAxisSpacing: 16,
+                    ) ,
+                    itemBuilder: (context, index){
+                       return  CategoryCard(categoryData: categoryController.categoryModel.data![index],);
+                    }
+                ),
+              );
+            }
           ),
         ),
       ),
